2b6490f46895d25dec24edead1bd15fe93ab9004
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-11-14 15:05:40 -0700

    chore: move commitlint and jest config to manifest

--- jest-tslint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["ts", "js"],
-  runner: "./dist/index.js",
-  testMatch: ["**/*.ts", "**/*.js"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
-};
